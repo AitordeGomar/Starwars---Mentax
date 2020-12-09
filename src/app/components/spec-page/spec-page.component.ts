bec21afe7f662b3b443e849ec768d64345c55357
Aitor <aitordegomar91@hotmail.com>
2020-12-09 08:08:03 +0100

    getting number from url after clicking

--- src/app/components/spec-page/spec-page.component.ts
+++ src/app/components/spec-page/spec-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { setMaxListeners } from 'process';
 import {ApiService} from '../../services/api.service';
 
 @Component({
@@ -12,20 +13,24 @@ export class SpecPageComponent implements OnInit {
   type;
   id;
   searchedPage;
+  result;
   
 
-  constructor(private swapi:ApiService, private router:Router, private actRoute: ActivatedRoute) { }
+  constructor(private swapi:ApiService, private router:Router, private actRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.searchedPage = "";
     this.type = this.actRoute.snapshot.params['type'];
     this.id = this.actRoute.snapshot.params['id'];
     this.swapi.getPage(this.type,this.id).subscribe((data)=>{
       this.searchedPage = data});
     this.currentPage = parseInt(this.id);
+    
   }
 
   toSpecElement(param){
-    this.router.navigate([this.type+"/"+param])
+    this.result = this.searchedPage.results[param].url.toString().slice(-3,-1).split("/").pop();
+    this.router.navigate([this.type+"/"+this.result])
   }
 
   prevPage(){
@@ -42,7 +47,7 @@ export class SpecPageComponent implements OnInit {
     
     this.router.navigate([this.type+"/page", this.currentPage+1]);
     this.currentPage++;
-    console.log(this.type);
+    console.log(this.searchedPage.results[0].url.toString().slice(-3,-1).split("/").pop()); 
   }
 
 }
